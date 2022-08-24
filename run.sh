ad417eef893fa1628b856232835afab62bf38821
ZElaXan <107149635+ElaXan@users.noreply.github.com>
2022-08-24 07:09:05 +0700

    export ANDROID_SDK (Ubuntu?)

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Android SDK PATH (Linux/Ubuntu)
+# export ANDROID_HOME=/home/ElaXan/Android/Sdk
+
 # Make Release
 ./gradlew assembleRelease
 
@@ -7,7 +10,8 @@
 # ./gradlew clean
 
 # Fake Signed
-# java -jar tool/uber-apk-signer.jar -a app/build/outputs/apk/release/app-release-unsigned.apk
+# Auto Sign is good...
+java -jar tool/uber-apk-signer.jar -a app/build/outputs/apk/release/app-release-unsigned.apk
 
 # Patch mod metadata to patch proxy
 # java -jar tool/lspatch.jar apk/GenshinImpact_v2.8.0_mod.apk -m app/build/outputs/apk/release/app-release-aligned-debugSigned.apk --v2
